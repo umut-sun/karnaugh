2b5fa196eeb8f06034e4dd3fd21a691964d4e7e9
Umut Sun <umut.sun@outlook.com.tr>
2018-10-30 15:07:26 +0200

    Update equation.js
    yorum satırlarıyla açıklama yapıldı

--- Resources/assets/js/equation.js
+++ Resources/assets/js/equation.js
@@ -1,12 +1,15 @@
 // count number of variables within the expression
 function CountVar( s ) {
 	var chars = {}, rv = '';
-	var s = s.replace(/[^a-zA-Z]/g, "");
+	var s = s.replace(/[^a-zA-Z]/g, "");   //a'dan z'ye A'dan Z'ye global arama yapar(küçük büyük harfe dikkat etmemesi için gi)
 
 	for (var i = 0; i < s.length; ++i) {
-		if (!(s[i] in chars)) {
+		if (!(s[i] in chars)) { //eğer girdinin i. karakteri chars dizisinde yoksa 
 			chars[s[i]] = 1;
-      		rv += s[i];
+			//console.log(s.length);
+			console.log(chars);
+      		rv += s[i]; //rv stringine s'nin i. karakterini ekler
+			console.log(rv);
 		}
 	}
 
@@ -20,6 +23,7 @@ function replaceVar (expression) {
 	for (var i = 0; i < TruthTable.length; i++) {
 
 		string = expression.replace(/a/g, TruthTable[i][0].Variable);
+		console.log(string);
 		string = string.replace(/b/g, TruthTable[i][1].Variable);
 		string = string.replace(/c/g, TruthTable[i][2].Variable);
 		string = string.replace(/d/g, TruthTable[i][3].Variable);
@@ -37,47 +41,65 @@ document.getElementById('equation').addEventListener('change', function() {
 	
 
 	// check if input is valid
-	if (isNaN(this.value) ){
+	if (isNaN(this.value) && (this.value).match(/[0-9]/)==null ){ //girilen değer sayı değilse işleme al
 		var strlower = this.value.toLowerCase();
 
 		console.log(strlower);
-		
+		//countvar fonksiyonuna gider
 		var varNum = (CountVar(this.value));
+        //a-z olarak girilen harf sayısını döner
 
-
-		var func = strlower.split("+");
+		var func = strlower.split("+"); //aralarında + varsa onları bölüyor
+		console.log("func bu ",func);
+		
+		
 		for (var i = 0; i < func.length; i++) {
 
-			func[i] = func[i].trim();
+			func[i] = func[i].trim(); //boşluklarını siler
 			
 			// put times(*)symbol between variables
-			func[i] = func[i].split(/([a-z])/).join("&").replace("&", "").split("&&").join("&").split("&!&").join("&!");
-
+			//func[i] = func[i].split(/([a-z])/).join("&").replace("&", "").split("&&").join("&").split("&!&").join("&!");  //a-z tüm karakterleri ayırıp aralarına & ekleyip eğer ki 
+			func[i] = func[i].split(/([a-z])/); //parçalara ayırdı
+			console.log("split",func[i]); 
+			func[i] = func[i].join("&"); //hepsini arasına & ekleyerek geri birleştirdi
+			console.log("join",func[i]);
+			func[i] = func[i].replace("&",""); //ilk & işaretini hiçbirşeye çevirdi
+			console.log("replace",func[i]);
+			func[i] = func[i].split("&&"); //arasında && bulunanları parçalara ayırdı
+			console.log("split",func[i]);
+			func[i] = func[i].join("&"); //ayırdıklarını arasına & ekleyerek birleştirdi
+			console.log("join",func[i]);
+			func[i] = func[i].split("&!&"); // arasında &!& bulunanları parçalara ayırdı
+			console.log("split",func[i]);
+			func[i] = func[i].join("&!"); // onları  &! şeklinde tekrar birleştirdi
+			console.log("join",func[i]);
 			// r_trim to remove * from the final of the string
-			func[i] = func[i].substr(0,(func[i].length -1));
+			func[i] = func[i].substr(0,(func[i].length -1));  //sondaki & karakterini sildi
+			console.log(func[i]);
+			console.log("");
 		};
 
-		strlower = func.join(" | ");
-
-		switch(varNum)
+		strlower = func.join(" | ");  // bulduğu func stringlerini aralarına | ekleyerek birleştirdi
+		console.log(strlower);
+		switch(varNum)  //harf sayısını kontrol ediyor 
 		{
-			case 4:
+			case 4:  //4 ise otomatik olarak 4 değişkeni seçiyor
 				document.getElementById('FourVariableRB').click();
 					replaceVar(strlower);
 			  	break;
 
-			case 3:
+			case 3:  //3 ise otomatik olarak 3 değişkeni seçiyor
 				document.getElementById('ThreeVariableRB').click();
 					replaceVar(strlower);
 		  		break;
 
-			default:
-				if (varNum < 3) {
+			default: //3 veya 4 değilse 
+				if (varNum < 3) {   //3'ten küçükse iki değişkeni seçiyor
 
 					document.getElementById('TwoVariableRB').click();
 					replaceVar(strlower);
 
-				}else if (varNum > 4){
+				}else if (varNum > 4){  //4'ten büyükse yanlış giriş uyarısı veriyor
 					alert("Invalid input");
 				};
 		}
